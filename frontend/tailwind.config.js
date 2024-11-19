ae126165ed52177f6df12e94663e22588c029d23
SantiSL5 <santiagosolerllin@gmail.com>
2024-11-19 05:04:44 +0100

    Flowbite, NextUI added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
+const flowbite = require("flowbite-react/tailwind");
+const {nextui} = require("@nextui-org/react");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    flowbite.content(),
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+    nextui(),
+  ],
 }
 
